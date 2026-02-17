b35c3c5cf48b4221bca70558a4f21dfe2d25c23e
josefrlnc01 <josefrlnc01@gmail.com>
2026-02-17 17:22:15 +0100

    modificación de comprobación de duración

--- backend/utils/getVideoMinutes.ts
+++ backend/utils/getVideoMinutes.ts
@@ -1,7 +1,12 @@
 export function getVideoMinutes(data:string){
     const indexM:number = data.indexOf("M")
-    
+    console.log(data)
     const minute = data[indexM - 1]
-    return minute
-   
+
+    //Si el formato pthms tiene mas de 7 caracteres es mayor a 10 min y no continuamos el flujo normal
+    if (data.length > 7) {
+        throw new Error("Video demasiado largo")
+    } else {
+        return minute
+    }
 }
