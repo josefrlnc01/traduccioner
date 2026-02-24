63f167dd62eae18606ce3c3bcf97b04ec0053dcf
josefrlnc01 <josefrlnc01@gmail.com>
2026-02-24 21:12:27 +0100

    mejora de middleware de cors

--- backend/middlewares/corsOptions.ts
+++ backend/middlewares/corsOptions.ts
@@ -1,16 +1,39 @@
 import cors from 'cors'
+export const isProd = process.env.NODE_ENV === 'production'
+
+function getRequiredEnv(key:string): string {
+    const value = process.env[key]
+    if (!value) {
+        throw new Error ("Variables de entorno no definidas")
+    }
+    return value
+}
+
+const backendUrl = isProd 
+    ? getRequiredEnv('PROD_BACKEND_URL') 
+    : getRequiredEnv('LOCAL_BACKEND_URL')   
+const frontendUrl = isProd 
+    ? getRequiredEnv('PROD_FRONTEND_URL')
+    : getRequiredEnv('LOCAL_FRONTEND_URL')
+
 
 export const corsMiddleware = () => cors({
     origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
-        const ACCEPTED_ORIGINS:string[] = [
-        'http://localhost:8000',
-        'http://localhost:5173'
+        const ACCEPTED_ORIGINS:string[] | undefined = [
+        backendUrl,
+        frontendUrl
     ]
+    if (process.env.NODE_ENV === 'production' && ACCEPTED_ORIGINS.length === 0) {
+        console.warn('Faltan las variables de produccion por definir')
+        return callback(null, true)
+    }
+
     if (!origin) return callback(null, true)
     if (ACCEPTED_ORIGINS.includes(origin as string)) {
         callback(null, true)
     } else {
-        callback(new Error('Not allowed by CORS'))
+        callback(new Error('No permitido por cors'))
     }
     }
 })
+
