4d0146df424cc1c05a66f1cd2fe38f826bc1a208
josefrlnc01 <josefrlnc01@gmail.com>
2026-02-25 16:23:53 +0100

    cambio de lógica en inicio del servidor

--- backend/api/server.ts
+++ backend/api/server.ts
@@ -4,7 +4,7 @@ dotenv.config()
 import { mainRoute } from '../routes/mainRoute.js'
 import { corsMiddleware } from '../middlewares/corsOptions.js'
 
-
+const isProd = process.env.NODE_ENV === 'production';
 const port = process.env.PORT 
 
 const app = express()
@@ -13,6 +13,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true })) 
 
 app.use('/link', mainRoute)
+
+if (!isProd) {
+    app.listen(port, () => {
+        console.log(`Sevidor corriendo en ${port}`)
+    })
+}
 
 export default app
 
