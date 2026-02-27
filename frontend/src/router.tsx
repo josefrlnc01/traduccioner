118268898c80b24ff1c715ddb94b0ed7255a7df0
josefrlnc01 <josefrlnc01@gmail.com>
2026-02-27 16:19:09 +0100

    adición del auth layout al router

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router"
 import AppLayout from "./layouts/AppLayout"
 import MainView from "./views/MainView"
+import AuthLayout from "./layouts/AuthLayout"
+import ConfirmAccountView from "./views/ConfirmAccountView"
 
 export default function Router() {
   return (
     <BrowserRouter>
         <Routes>
             <Route element={<AppLayout/>}>
-                <Route path="/" element={<MainView/>} index></Route>
+                <Route path="/" element={<MainView/>} index/>
+            </Route>
+            <Route element={<AuthLayout/>}>
+              <Route path="/auth/confirm-account" element={<ConfirmAccountView/>}/>
             </Route>
         </Routes>
     </BrowserRouter>
