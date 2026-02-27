7598ab550e87ef7a50f7ed7d99681ae65a1ca70e
josefrlnc01 <josefrlnc01@gmail.com>
2026-02-27 11:27:20 +0100

    creación de modelo de usuario con mongoose

---
+++ backend/models/User.ts
@@ -0,0 +1,28 @@
+import mongoose, {Schema, Document} from 'mongoose'
+
+export interface IUser extends Document {
+    name: string,
+    email: string,
+    password: string
+}
+
+export const userSchema: Schema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+
+const User = mongoose.model<IUser>('User', userSchema)
+
+export default User
