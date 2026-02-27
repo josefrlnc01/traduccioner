4bb3a1cc9a4cca2fcf60b1acca0da148c6fb4988
josefrlnc01 <josefrlnc01@gmail.com>
2026-02-27 16:22:30 +0100

    configuración de useAuth en AppLayout y contenidos internos

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
 
+import { useAuth } from 'src/hooks/useAuth'
+import { Link, Navigate } from 'react-router'
+import { Outlet } from 'react-router'
 export default function AppLayout() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const {data, isError, isLoading} = useAuth()
+
+  if (isLoading) return 'Cargando...'
+  
+  if (isError) <Navigate to={'/auth/login'}/>
+
+
+  if (data) {
+    return (
+    <>
+    <header
+    className='bg-gray-800 py-5'
+    >
+        <div className='max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center'>
+
+            <div className='w-64'>
+                <Link to={'/'}>
+                
+                </Link>
+                
+            </div>
+
+        </div>
+
+        
+    </header>
+
+    <section className='max-w-screen-2xl mx-auto mt-10 p-5'>
+        <Outlet/>
+    </section>
+    
+
+    <footer className='py-5'>
+        <p className='text-center'>Derechos reservados</p>
+    </footer>
+    
+    
+    </>
+)
+  }
 }
