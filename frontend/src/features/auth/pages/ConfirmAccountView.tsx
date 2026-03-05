7618770797dd70e458f4b812046f6194a87c130c
josefrlnc01 <josefrlnc01@gmail.com>
2026-03-05 17:15:10 +0100

    cambios visuales vista de confirmación de token

--- frontend/src/features/auth/pages/ConfirmAccountView.tsx
+++ frontend/src/features/auth/pages/ConfirmAccountView.tsx
@@ -11,7 +11,6 @@ import { InputOTP, InputOTPGroup, InputOTPSlot } from "@/shared/components/ui/in
 export default function ConfirmAccountView() {
   const [token, setToken] = useState<TokenConfirmation['token']>('')
 
-
   const { mutate } = useMutation({
     mutationFn: confirmAccount,
     onError: (error) => {
@@ -32,24 +31,29 @@ export default function ConfirmAccountView() {
 
   return (
     <>
-      <InputOTP value={token} maxLength={6} onChange={handleChange} onComplete={handleComplete}>
-        <InputOTPGroup>
-          <InputOTPSlot index={0} />
-          <InputOTPSlot index={1} />
-          <InputOTPSlot index={2} />
-          <InputOTPSlot index={3} />
-          <InputOTPSlot index={4} />
-          <InputOTPSlot index={5} />
-        </InputOTPGroup>
-      </InputOTP>
-      <nav className="mt-10 flex flex-col space-y-4">
-        <Link
-          to='/auth/request-code'
-          className="text-center text-gray-300 font-normal"
-        >
-          Solicitar un nuevo Código
-        </Link>
-      </nav>
+      <section 
+      className="max-w-md mx-auto space-y-6 p-8 flex flex-col justify-center items-center">
+        <aside className="p-12 bg-zinc-950 border border-zinc-800 rounded-2xl shadow-xl">
+        <InputOTP value={token} maxLength={6} onChange={handleChange} onComplete={handleComplete}>
+          <InputOTPGroup className="text-white font-bold">
+            <InputOTPSlot index={0} />
+            <InputOTPSlot index={1} />
+            <InputOTPSlot index={2} />
+            <InputOTPSlot index={3} />
+            <InputOTPSlot index={4} />
+            <InputOTPSlot index={5} />
+          </InputOTPGroup>
+        </InputOTP>
+        <nav className="mt-10 flex flex-col space-y-4">
+          <Link
+            to='/auth/request-code'
+            className="text-center text-gray-300 font-normal"
+          >
+            Solicitar un <span className="text-indigo-400 font-medium">nuevo código</span>
+          </Link>
+        </nav>
+        </aside>
+      </section>
     </>
 
   )
