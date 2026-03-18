c8b36d3065935987aad6d122ed810f69d642bb22
josefrlnc01 <josefrlnc01@gmail.com>
2026-03-18 16:54:58 +0100

    sección pricing

---
+++ frontend/src/landing/components/Pricing.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Pricing() {
+    return (
+        <section>
+            <aside>
+                <h3 className='text-3xl text-white font-bold'>Planes diseñados a medida</h3>
+                <p className='max-w-2xl mx-auto text-sm md:text-lg text-gray-500 text-center leading-relaxed'>Escoge lo que mejor se adapte a tus necesidades.</p>
+            </aside>
+            <aside>
+                <div>
+                    <h5></h5>
+                    <p></p>
+                    <ul>
+                        <li>
+
+                        </li>
+                        <li>
+                            
+                        </li>
+                        <li>
+                            
+                        </li>
+                    </ul>
+                </div>
+            </aside>
+        </section>
+    )
+}
