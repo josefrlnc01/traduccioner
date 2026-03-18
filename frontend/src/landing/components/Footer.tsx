0a4d59f609b20e06d44af48c52ac3d0fd06a880e
josefrlnc01 <josefrlnc01@gmail.com>
2026-03-18 16:41:35 +0100

    sección footer

---
+++ frontend/src/landing/components/Footer.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className='w-screen  border-b border-slate-800 grow-0'>
+            <aside className='w-screen pt-5 pb-7 pr-14 pl-14 flex flex-col gap-8'>
+                <div className='w-full flex flex-wrap justify-between gap-8 text-sm text-gray-400'>
+                    <div className='grow-0'>
+                        <h2 className='text-lg font-bold self-start'>Transcriber AI</h2>
+                    </div>
+                    <div>
+                        <nav className='flex gap-2 text-md'>
+                            <a className='hover:text-white transition-colors' href=''>Términos</a>
+                            <a className='hover:text-white transition-colors' href=''>Soporte</a>
+                            <a className='hover:text-white transition-colors' href=''>Privacidad</a>
+                            <a className='hover:text-white transition-colors' href=''>Contacto</a>
+                        </nav>
+                    </div>
+
+                </div>
+                <div className='border border-s border-gray-900/90 w-full'></div>
+                <div className='w-full flex flex-wrap justify-between gap-8 text-sm text-gray-400'>
+                    <small className='text-gray-400'> ©2026  Transcriber AI. Transcribe videos y audios. </small>
+                    <span>Desarrollado por José María Sánchez Serna</span>
+                </div>
+            </aside>
+
+        </footer>
+    )
+}
--- frontend/src/landing/pages/LandingPage.tsx
+++ frontend/src/landing/pages/LandingPage.tsx
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import Hero from '../components/Hero'
 import FeaturesSection from '../components/FeaturesSection'
 import HowItWorks from '../components/HowItWorks'
+import Footer from '../components/Footer'
 
 export default function LandingPage() {
   return (
@@ -11,6 +12,7 @@ export default function LandingPage() {
     <Hero/>
     <FeaturesSection/>
     <HowItWorks/>
+    <Footer/>
     </>
     
   )
