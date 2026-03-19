c17a4987030a083919e54fa1ea3c19d6a9b06491
josefrlnc01 <josefrlnc01@gmail.com>
2026-03-19 17:41:24 +0100

    eliminación función innecesaria

--- backend/modules/file/file.service.ts
+++ backend/modules/file/file.service.ts
@@ -12,33 +12,6 @@ import Quota from '../quota/quota.schema.js'
 
 
 export class FileService {
-    static createPathForFile = async (file: Express.Multer.File) => {
-        try {
-            const __fileName = fileURLToPath(import.meta.url)
-            const __dirname = dirname(__fileName)
-            const rutaArchivo = path.join(__dirname, '..', '..', 'archivos.json')
-            const fileName = file.filename
-            const fileIn = await fileExists()
-            if (!fileIn) {
-                await fs.writeFile(rutaArchivo, JSON.stringify([]), "utf-8")
-            }
-            //Creación de archivo de control de audios del usuario
-            const archivo = await fs.readFile(rutaArchivo, "utf-8")
-            const archivoParseado = JSON.parse(archivo)
-
-            const nombreArchivo = {
-                nombre: fileName
-            }
-
-            //Creación de ruta de audios introducidos por el usuario
-            const filePath = path.join(process.cwd(), 'uploads', fileName)
-            archivoParseado.push(nombreArchivo)
-            await fs.writeFile(rutaArchivo, JSON.stringify(archivoParseado, null, 2), "utf-8")
-            return filePath
-        } catch {
-            throw new Error('Hubo un error al crear el archivo de nombres')
-        }
-    }
 
     static insertTranscription = async ({ data, user }: InsertFileTranscriptionProps) => {
         try {
