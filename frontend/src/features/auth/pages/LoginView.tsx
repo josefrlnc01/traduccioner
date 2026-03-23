cb0a74c81ed098a5625acdf41f3d3af46a8ab12f
josefrlnc01 <josefrlnc01@gmail.com>
2026-03-23 11:45:00 +0100

    cambios visuales login

--- frontend/src/features/auth/pages/LoginView.tsx
+++ frontend/src/features/auth/pages/LoginView.tsx
@@ -11,7 +11,7 @@ type UserLoginForm = {
     password: string
 }
 
-export default function LoginView() { 
+export default function LoginView() {
 
     const navigate = useNavigate()
     const initialValues: UserLoginForm = {
@@ -56,78 +56,111 @@ export default function LoginView() {
 
     return (
         <>
-            <form 
-    className='max-w-md mx-auto space-y-6 p-8 bg-zinc-950 border border-zinc-800 rounded-2xl shadow-xl' 
-    onSubmit={handleSubmit(handleLogin)}
-    noValidate
->
-    {/* Header o Título opcional */}
-    <div className="text-center mb-8">
-        <h2 className="text-3xl font-bold text-white">Bienvenido</h2>
-        <p className="text-zinc-400 mt-2">Ingresa tus credenciales para acceder</p>
-    </div>
+            <form
+                className='max-w-md mx-auto space-y-6 p-8 bg-zinc-950 border border-zinc-800 rounded-2xl shadow-xl'
+                onSubmit={handleSubmit(handleLogin)}
+                noValidate
+            >
+                {/* Header o Título opcional */}
+                <div className="text-center mb-8">
+                    <h2 className="text-3xl font-bold text-white">Aud<span className='text-blue-600/80'>Wave</span></h2>
+                    <p className="text-zinc-400 mt-2">Transcribe audio con IA</p>
+                </div>
 
-    <div className='flex flex-col gap-2'>
-        <label className='text-sm font-semibold text-zinc-300 ml-1' htmlFor='email'>
-            Correo Electrónico
-        </label>
-        <input
-            id='email'
-            type='email'
-            placeholder="ejemplo@correo.com"
-            className='w-full p-3 bg-zinc-900 border border-zinc-700 rounded-lg text-white placeholder-zinc-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition-all'
-            {...register("email", {
-                required: 'El email es obligatorio',
-                pattern: {
-                    value: /\S+@\S+\.\S+/,
-                    message: "E-mail no válido",
-                },
-            })} 
-        />
-        {errors.email && (
-            <ErrorMessage>{errors.email.message}</ErrorMessage>
-        )}
-    </div>
+                <button
+                    type='submit'
+                    onClick={handleLoginGoogle}
+                    className='w-full py-3 rounded-xl
+                    bg-white text-gray-800
+                    font-medium
+                    flex items-center justify-center gap-3
+                    hover:bg-gray-100 hover:scale-[1.02] transition cursor-pointer'
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="20" height="20">
+                        <path fill="#FFC107" d="M43.611 20.083h-1.611V20H24v8h11.303C33.91 32.657 29.354 36 24 36c-6.627 0-12-5.373-12-12s5.373-12 12-12c3.059 0 5.842 1.154 7.964 3.036l5.657-5.657C34.053 6.053 29.278 4 24 4 12.955 4 4 12.955 4 24s8.955 20 20 20 20-8.955 20-20c0-1.341-.138-2.651-.389-3.917z" />
+                        <path fill="#FF3D00" d="M6.306 14.691l6.571 4.819C14.655 16.108 18.961 13 24 13c3.059 0 5.842 1.154 7.964 3.036l5.657-5.657C34.053 6.053 29.278 4 24 4 16.318 4 9.656 8.337 6.306 14.691z" />
+                        <path fill="#4CAF50" d="M24 44c5.235 0 9.96-2.009 13.527-5.273l-6.245-5.285C29.201 34.889 26.715 36 24 36c-5.333 0-9.876-3.317-11.29-7.946l-6.523 5.025C9.503 39.556 16.227 44 24 44z" />
+                        <path fill="#1976D2" d="M43.611 20.083h-1.611V20H24v8h11.303c-1.106 3.303-3.343 5.936-6.021 7.442l.01-.007 6.245 5.285C33.29 42.091 44 36 44 24c0-1.341-.138-2.651-.389-3.917z" />
+                    </svg>
+                    Continuar con Google</button>
 
-    <div className='flex flex-col gap-2'>
-        <label className='text-sm font-semibold text-zinc-300 ml-1' htmlFor='password'>
-            Contraseña
-        </label>
-        <input 
-            id='password'
-            type='password'
-            placeholder="••••••••"
-            className='w-full p-3 bg-zinc-900 border border-zinc-700 rounded-lg text-white placeholder-zinc-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition-all'
-            {...register('password', {
-                required: 'La contraseña es obligatoria',
-            })}
-        />
-        {errors.password && (
-            <ErrorMessage>{errors.password.message}</ErrorMessage>
-        )}
-    </div>
-
-    <input 
-        type='submit'
-        value='Iniciar sesión'
-        className='w-full p-3 mt-4 cursor-pointer bg-white hover:bg-zinc-200 text-black font-bold rounded-lg transition-colors shadow-lg'
-    />
-    <button
-    type='submit'
-    onClick={handleLoginGoogle}
-        className='w-full p-3 mt-4 cursor-pointer bg-white hover:bg-zinc-200 text-black font-bold rounded-lg transition-colors shadow-lg'
-    >Google</button>
+                <div className="flex items-center gap-4 my-6 text-slate-500">
+                    <div className="flex-1 h-px bg-slate-700"></div>
+                    <span className="text-sm">o</span>
+                    <div className="flex-1 h-px bg-slate-700"></div>
+                </div>
 
 
-    <nav className='mt-6 flex flex-col items-center'>
-        <Link
-            className="text-sm text-zinc-400 hover:text-white transition-colors"
-            to={'/auth/register'}
-        >
-            ¿No tienes cuenta? <span className="text-indigo-400 font-medium">Crea una aquí</span>
-        </Link>
-    </nav>
-</form>
+                <div className='flex flex-col gap-2'>
+                    <label className='text-sm font-semibold text-zinc-300 ml-1' htmlFor='email'>
+                        Email
+                    </label>
+                    <input
+                        id='email'
+                        type='email'
+                        placeholder="ejemplo@correo.com"
+                        className='w-full px-4 py-3 rounded-xl
+                        bg-slate-900/60
+                        border border-slate-700
+                        focus:border-blue-500
+                        focus:ring-2 focus:ring-blue-500/30
+                        outline-none
+                        transition text-white'
+                        {...register("email", {
+                            required: 'El email es obligatorio',
+                            pattern: {
+                                value: /\S+@\S+\.\S+/,
+                                message: "E-mail no válido",
+                            },
+                        })}
+                    />
+                    {errors.email && (
+                        <ErrorMessage>{errors.email.message}</ErrorMessage>
+                    )}
+                </div>
+
+                <div className='flex flex-col gap-2'>
+                    <label className='text-sm font-semibold text-zinc-300 ml-1' htmlFor='password'>
+                        Contraseña
+                    </label>
+                    <input
+                        id='password'
+                        type='password'
+                        placeholder="••••••••"
+                        className='w-full px-4 py-3 rounded-xl
+                        bg-slate-900/60
+                        border border-slate-700
+                        focus:border-blue-500
+                        focus:ring-2 focus:ring-blue-500/30
+                        outline-none
+                        transition text-white'
+                        {...register('password', {
+                            required: 'La contraseña es obligatoria',
+                        })}
+                    />
+                    {errors.password && (
+                        <ErrorMessage>{errors.password.message}</ErrorMessage>
+                    )}
+                </div>
+
+                <input
+                    type='submit'
+                    value='Iniciar sesión'
+                    className='w-full py-3 rounded-xl
+                    bg-gradient-to-r from-blue-500 to-indigo-500
+                    text-white font-semibold
+                    hover:scale-[1.02] transition cursor-pointer'
+                />
+
+                <nav className='mt-6 flex flex-col items-center'>
+                    <Link
+                        className="text-sm text-zinc-400 hover:text-white transition-colors"
+                        to={'/auth/register'}
+                    >
+                        ¿No tienes cuenta? <span className="text-indigo-400 font-medium">Crea una aquí</span>
+                    </Link>
+                </nav>
+            </form>
         </>
     )
 }
