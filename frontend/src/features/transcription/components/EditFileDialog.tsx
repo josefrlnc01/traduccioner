5df8c9bfcb8d5f63c396615431f89a14c97f3fe8
josefrlnc01 <josefrlnc01@gmail.com>
2026-03-26 13:34:50 +0100

    cambio de estilos en dialogo de edición de archivo

--- frontend/src/features/transcription/components/EditFileDialog.tsx
+++ frontend/src/features/transcription/components/EditFileDialog.tsx
@@ -18,20 +18,20 @@ type EditFileDialogProps = {
     id: string,
     title: string
 }
-export default function EditFileDialog({isOpen, setIsOpen, id, title}: EditFileDialogProps) {
+export default function EditFileDialog({ isOpen, setIsOpen, id, title }: EditFileDialogProps) {
     const [newTitle, setNewTitle] = useState(title)
     const queryClient = useQueryClient()
     const editTitleFN = useMutation({
-        mutationFn:editTitle,
+        mutationFn: editTitle,
         onSuccess: (data) => {
-            queryClient.invalidateQueries({queryKey: ['saveds']})
+            queryClient.invalidateQueries({ queryKey: ['saveds'] })
             toast.success(data)
         },
         onError: (error) => {
             toast.error(error.message)
         }
-    }) 
-    
+    })
+
     const handleTitle = (e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>) => {
         setNewTitle(e.target.value)
     }
@@ -46,25 +46,34 @@ export default function EditFileDialog({isOpen, setIsOpen, id, title}: EditFileD
     }
 
     console.log('isopen', isOpen)
-  return (
-    <>
-    <Dialog open={isOpen} onOpenChange={setIsOpen}>
-    <DialogTrigger asChild>
-        <button>Abrir</button>
-    </DialogTrigger>
-    <DialogContent className='bg-white'>
-        <DialogHeader>
-            <DialogTitle>Nuevo título</DialogTitle>
-            <DialogDescription>
-                <input onChange={handleTitle} type='text' value={newTitle} className='w-full p-2'/>
-            </DialogDescription>
-        </DialogHeader>
-        <DialogFooter>
-            <button className='flex items-center gap-1.5 px-3 py-1.5 bg-red-600/90 text-slate-300 hover:text-white text-xs font-medium rounded-lg transition-colors border border-slate-600/50 cursor-pointer' onClick={() => setIsOpen(false)}>Cancelar</button>
-            <button className='flex items-center gap-1.5 px-3 py-1.5 bg-blue-700/90 text-slate-300 hover:text-white hover:bg-blue-600 text-xs font-medium rounded-lg transition-colors border border-slate-600/50 cursor-pointer' onClick={handleEdit}>Actualizar</button>
-        </DialogFooter>
-    </DialogContent>
-</Dialog>
-    </>
-  )
+    return (
+        <>
+            <Dialog open={isOpen} onOpenChange={setIsOpen}>
+                <DialogTrigger asChild>
+                    <button>Abrir</button>
+                </DialogTrigger>
+                <DialogContent className='bg-slate-800'>
+                    <DialogHeader>
+                        <DialogTitle className='text-white mb-3'>Nuevo título</DialogTitle>
+                        <DialogDescription>
+                            <input onChange={handleTitle} type='text' value={newTitle} className='
+                        w-full 
+                        p-3 
+                        text-gray-200 
+                        bg-slate-900/60
+                        border border-slate-700
+                        focus:border-blue-800
+                        focus:ring-2 focus:ring-blue-500/30
+                        outline-none
+                        rounded-md'/>
+                        </DialogDescription>
+                    </DialogHeader>
+                    <DialogFooter>
+                        <button className='flex items-center gap-1.5 px-3 py-1.5 bg-red-600/90 text-slate-300 hover:text-white text-xs font-medium rounded-lg transition-colors border border-slate-600/50 cursor-pointer' onClick={() => setIsOpen(false)}>Cancelar</button>
+                        <button className='flex items-center gap-1.5 px-3 py-1.5 bg-blue-700/90 text-slate-300 hover:text-white hover:bg-blue-600 text-xs font-medium rounded-lg transition-colors border border-slate-600/50 cursor-pointer' onClick={handleEdit}>Actualizar</button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        </>
+    )
 }
