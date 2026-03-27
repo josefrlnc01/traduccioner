d4b542fa786bf63ea1cce377723d21433755141b
josefrlnc01 <josefrlnc01@gmail.com>
2026-03-27 10:59:59 +0100

    media queries

--- frontend/src/features/transcription/components/SavedsList.tsx
+++ frontend/src/features/transcription/components/SavedsList.tsx
@@ -31,26 +31,22 @@ export default function SavedsList() {
     const youtubeFiles: Saveds = data.youtubeFiles
     return (
     <aside className='w-full min-w-full p-4 md:p-0 md:w-3/4 md:min-w-3/4 lg:1/2 lg:min-w-1/2 m-auto flex flex-col justify-center items-center mb-10'>
-
       <div className='w-full flex flex-col gap-4'>
-
         {files.map(file => (
           <button key={file._id}>
-            <Link to={`/saveds/${file.fileId}`} className='bg-slate-800/70 p-6 rounded-md hover:bg-slate-700/90 flex justify-between items-center hover:scale-105 transition-all duration-200 ease cursor-pointer' key={file._id}>
-              <span>{file.title}</span>
-              <span className='text-white min-w-18 flex justify-center items-center bg-blue-600 pt-1 pb-1 pl-3 pr-3 rounded-2xl'>{file.duration}</span>
+            <Link to={`/saveds/${file.fileId}`} className='bg-slate-800/70 p-4 md:p-6 gap-2 rounded-md hover:bg-slate-700/90 flex justify-between items-center hover:scale-105 transition-all duration-200 ease cursor-pointer' key={file._id}>
+              <h4 className='wrap-break-word text-start text-sm md:text-md'>{file.title}</h4>
+              <span className='text-white min-w-22 text-xs md:text-sm flex justify-center items-center bg-blue-600 pt-1 pb-1 pl-3 pr-3 rounded-2xl'>{file.duration}</span>
             </Link>
           </button>
         ))}
         {youtubeFiles.map(file => (
           <button key={file._id}>
-            <Link to={`/saveds/${file.fileId}`} className='bg-slate-800/70 p-6 rounded-md hover:bg-slate-700/90 flex justify-between items-center hover:scale-105 transition-all duration-200 ease cursor-pointer' key={file._id}>
-              <span>{file.title}</span>
-              <span className='text-white min-w-18 flex justify-center items-center bg-blue-600 pt-1 pb-1 pl-3 pr-3 rounded-2xl'>{file.duration}</span>
+            <Link to={`/saveds/${file.fileId}`} className='bg-slate-800/70 p-4 md:p-6 gap-2 rounded-md hover:bg-slate-700/90 flex justify-between items-center hover:scale-105 transition-all duration-200 ease cursor-pointer' key={file._id}>
+              <h4 className='wrap-break-word text-start text-sm md:text-md'>{file.title}</h4>
+              <span className='text-white min-w-22 text-xs md:text-sm flex justify-center items-center bg-blue-600 pt-1 pb-1 pl-3 pr-3 rounded-2xl'>{file.duration}</span>
             </Link>
           </button>
-
-
         ))}
       </div>
     </aside>
